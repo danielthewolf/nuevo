4f87674cefed9d200e0beba2d6e7a31faf0b546a
danielthewolf <danieltesorero2009@hotmail.com>
2013-10-23 21:18:28 -0200

    calculadora

---
+++ CalculadoraPresentacion/src/calculadorapresentacion/CalculadoraPresentacion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorapresentacion;
+
+/**
+ *
+ * @author bentura
+ */
+public class CalculadoraPresentacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       InterfazCalculadora in=new InterfazCalculadora();
+        
+    }
+}
